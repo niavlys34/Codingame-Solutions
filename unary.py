bea37fb97025a172d116cc5a18117e4b1424dc69
Sylvain Soulier <64439113+niavlys34@users.noreply.github.com>
2022-12-09 21:20:44 +0100

    Update unary.py
    Commentaire plus développés

--- unary.py
+++ unary.py
@@ -7,13 +7,19 @@ message = input()
 
 for char in message:
     char_in_bin = f'{ord(char):b}'.zfill(7)
-    # char_in_bin: str. Example 0000001
+    # char_in_bin: str.
+    # Pour l'exemple, char = 'C'
+    #   ord(char) = 67
+    #   f'{ord(char):b}' = '1000011'
+    #   ici zfill(7) ne change rien
+    #   mais '1'.zfill(7) = '0000001' ;-)
     pre_code += char_in_bin
 
 seqs = re.findall(r"0+|1+", pre_code)
-# seqs: list. Example ['000000', '1']
+# seqs: list.
+# Pour l'exemple, seqs = ['1', '0000', '11']
 for seq in seqs:
     code.append('00' if seq[0] == '0' else '0')
     code.append('0' * len(seq))
 
-print(' '.join(code), end='')
+print(' '.join(code))

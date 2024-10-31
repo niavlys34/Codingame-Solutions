bc33dccad3f101a896165e564fe91e20e179698f
Sylvain Soulier <64439113+niavlys34@users.noreply.github.com>
2024-10-31 21:00:45 +0100

    Update horses.py
    Correction d'une faute d'orthographe dans les commentaires

--- horses.py
+++ horses.py
@@ -13,7 +13,7 @@ last_pi = 0
 for pi in pis:
     if (pi - last_pi) < gap:
         gap = pi - last_pi
-    # Supposont qu'il n'y aura jamais 2 puissances égales
+    # Supposons qu'il n'y aura jamais 2 puissances égales
     #  (ce n'est pas précisé dans la consigne)
     # 1 étant l'écart le plus faible, on peu s'arrêter et
     # éviter de continuer la boucle pour rien...
